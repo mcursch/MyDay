e381119993f59450a5cf91e1a0394d6ee55132a6
Matt Curschman <mcurschman@conferencecatalysts.com>
2024-01-26 21:26:55 -0500

    basic setup: tailwind installed, layout added

---
+++ myday/pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+</script>
+
+<template>
+  <MainLayout>
+    <div class="text-4xl text-blue-500">
+      Page: foo
+    </div>
+  </MainLayout>
+
+</template>
+
+<style scoped></style>
